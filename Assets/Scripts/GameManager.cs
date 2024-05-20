531c5156ffb1ec6752f292cddf54e9dbabf9c180
ayslan-gamedev <ayslan.gamedev@gmail.com>
2024-05-19 23:16:52 -0300

    ENGINE - Movement Class
    create a generic movement class to uses in all objects

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public abstract class GlobalValues
+{
+    public const float LimitX = 7;
+}
+
+public class GameManager : MonoBehaviour
+{
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public unsafe class Movement
+{
+    private readonly Transform _transform;
+    private readonly Vector2* _direction;
+
+    private readonly float* _speed;
+    
+    public Movement(Transform transform, Vector2 *direction, float *speed)
+    {
+        _transform = transform;
+        _direction = direction;
+        _speed = speed;
+    }
+    
+    /// <summary>
+    /// Move the object
+    /// </summary>
+    public void Move()
+    {
+        var direction = *_direction * *_speed;
+        _transform.Translate(direction);
+    }
+}
