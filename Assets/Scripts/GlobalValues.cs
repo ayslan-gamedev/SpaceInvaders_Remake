45156d510b5c90b9b30d5a2fda230b297e86d19c
ayslan-gamedev <ayslan.gamedev@gmail.com>
2024-05-22 14:44:34 -0300

    PLAY - Player Shot bullet
    Refact global values

---
+++ Assets/Scripts/GlobalValues.cs
@@ -0,0 +1,9 @@
+public abstract class GlobalValues
+{
+    public const float LimitMaxY = 10;
+    public const float LimitMinY = -5;
+    
+    public const float LimitMaxX = 7;
+    public const float LimitMinX = -7;
+
+}
---
+++ Assets/Scripts/ObjectInstantiated.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class ObjectInstantiated : MonoBehaviour
+{    
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private Vector2 direction = Vector2.up;
+
+    private Movement _movement;
+    
+    private unsafe void Start()
+    {
+        fixed (Vector2* vector2 = &direction)
+        fixed (float* speed = &moveSpeed)
+        {
+            _movement = new Movement(transform, vector2, speed);
+        }
+    }
+
+    private void Update()
+    {
+        var objectIsInsideGameArea = transform.position is 
+        { 
+            x: > GlobalValues.LimitMinX and < GlobalValues.LimitMaxY, 
+            y: > GlobalValues.LimitMinY and < GlobalValues.LimitMaxY 
+        };
+        
+        if(!objectIsInsideGameArea)
+        {
+            Destroy(gameObject);
+        }
+        
+        _movement.Move();
+    }
+}
