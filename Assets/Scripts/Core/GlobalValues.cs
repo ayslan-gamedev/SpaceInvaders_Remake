1b1fe436084f45e31a60fb6375f4a6840958c95d
ayslan-gamedev <ayslan.gamedev@gmail.com>
2024-05-23 20:13:00 -0300

    FIX - Unsafe Compatibilitie

---
+++ Assets/Scripts/Core/GlobalValues.cs
@@ -0,0 +1,12 @@
+namespace SpaceInvaders
+{
+    public abstract class GlobalValues
+    {
+        public const float LimitMaxY = 10;
+        public const float LimitMinY = -5;
+    
+        public const float LimitMaxX = 7;
+        public const float LimitMinX = -7;
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,56 +1,59 @@
 using System;
 using UnityEngine;
 
-public unsafe class Player : MonoBehaviour
+namespace SpaceInvaders
 {
-    [SerializeField] private float playerSpeed;
-    
-    private const string AxesHorizontal = "Horizontal";
-    
-    private Vector2 _inputDirection;
-    private Movement _movement;
-
-    private void Start()
+    public unsafe class Player : MonoBehaviour
     {
-        fixed (Vector2* pInputDirection = &_inputDirection)
-        fixed (float* pPlayerSpeed = &playerSpeed)
+        [SerializeField] private float playerSpeed;
+    
+        private const string AxesHorizontal = "Horizontal";
+    
+        private Vector2 _inputDirection;
+        private Movement _movement;
+
+        private void Start()
         {
-            _movement = new Movement(transform, pInputDirection, pPlayerSpeed);
+            fixed (Vector2* pInputDirection = &_inputDirection)
+            fixed (float* pPlayerSpeed = &playerSpeed)
+            {
+                _movement = new Movement(transform, pInputDirection, pPlayerSpeed);
+            }
         }
-    }
 
-    private void Update()
-    {
-        var playerMove = MovePlayer().IsOk;
+        private void Update()
+        {
+            var playerMove = MovePlayer().IsOk;
         
-        if (!playerMove)
-        {
-            Start();
+            if (!playerMove)
+            {
+                Start();
+            }
         }
-    }
     
-    private Result<bool, Exception> MovePlayer()
-    {
-        if (_movement is null)
+        private Result<bool, Exception> MovePlayer()
         {
-            return new Exception("_movement is null");
-        }
+            if (_movement is null)
+            {
+                return new Exception("_movement is null");
+            }
 
-        var playerInput = new Vector2(
-            x: Input.GetAxisRaw(AxesHorizontal),
-            y: 0);
-        _inputDirection = playerInput;
+            var playerInput = new Vector2(
+                x: Input.GetAxisRaw(AxesHorizontal),
+                y: 0);
+            _inputDirection = playerInput;
 
-        // Verify Borders
-        var isOnLimitMap = !(transform.position.x <= -GlobalValues.LimitX && playerInput.x < 0 ||    // player < X
-                             transform.position.x >= GlobalValues.LimitX && playerInput.x > 0);      // player > X
+            // Verify Borders
+            var isOnLimitMap = !(transform.position.x <= -GlobalValues.LimitMaxX && playerInput.x < 0 ||    // player < X
+                                 transform.position.x >= GlobalValues.LimitMinX && playerInput.x > 0);      // player > X
 
-        if (isOnLimitMap)
-        {
-            // Move the player
-            _movement.Move();
-        }
+            if (isOnLimitMap)
+            {
+                // Move the player
+                _movement.Move();
+            }
         
-        return true;
+            return true;
+        }
     }
 }
